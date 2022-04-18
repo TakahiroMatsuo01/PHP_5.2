57609234b25de3962e1c72d413aa1e89af9a5174
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-04-18 10:07:57 +0900

    課題実施後

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    print strtolower('WINGSプロジェクト<br/>');
+    print strtoupper("wingsプロジェクト<br/>");
+    print lcfirst('WINGS　PROJECT<br/>');
+    print ucwords('wings project<br/>');
+?>
+    </body>
+</html>
